ba7f246a631adbb0ee773808341176637d39b653
Felipe 0liveira <felpd3v@gmail.com>
2018-11-05 19:02:17 -0200

    FROM GIT LAB

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# sts-employers
-A Python Script to make your life easy!
+# Meu Holerite
+
+_Automatizar preenchimento de folha de ponto_
+
+## O Projeto
+
+Este foi criado a partir da dificuldade no preenchimento da folha de ponto na plataforma da STS Employers!<br>
+O script basicamente, gera horários randômicos para a início do expediente, saída para o almoço, retorno e fim do expediênte!
+
+## Versões
+
+|        | Version |
+| ------ | :-----: |
+| Python |    3.7.0    |
---
+++ index.py
@@ -0,0 +1,107 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import time
+
+browser = webdriver.Chrome('./selenium/chromedriver.exe')
+browser.get('http://meuholerite.com.br')
+
+# Data
+cpf = 'cpf'
+senha = 'password'
+notWorkingAt = ['05/10/2018'] # String array with dates (Eg.: 02/10/2018)
+workOnSaturday = False
+workOnSunday = False
+
+# Login
+txtUsuario = browser.find_element(By.ID, 'txtUsuario')
+txtUsuario.send_keys(cpf)
+
+Password = browser.find_element(By.ID, 'Password')
+Password.send_keys(senha)
+
+btnLogin = browser.find_element(By.ID, 'btnLogin')
+time.sleep(1)
+btnLogin.click()
+
+# Time & Attendance / Ponto
+browser.get('https://prd.meuholerite.com.br/Ponto.aspx')
+
+tdsDiaSemana = browser.find_elements(By.CSS_SELECTOR, '.tabela_ponto tbody tr .td_dia_semana')
+
+# Início do Expediente
+tdsEntrada1 = browser.find_elements(By.CSS_SELECTOR, '.tabela_ponto tbody tr .coluna1.borda_espessa_esquerda.td_entrada a')
+# Início do Almoço
+tdsSaida1 = browser.find_elements(By.CSS_SELECTOR, '.tabela_ponto tbody tr .coluna2.td_saida a')
+
+# Fim do Almoço
+tdsEntrada2 = browser.find_elements(By.CSS_SELECTOR, '.tabela_ponto tbody tr .coluna3.borda_espessa_esquerda.td_entrada a')
+# Fim do Expediente
+tdsSaida2 = browser.find_elements(By.CSS_SELECTOR, '.tabela_ponto tbody tr .coluna4.td_saida a')
+
+# Schedule Modal
+modalSchedule = browser.find_element(By.ID, 'ctl00_cphPrincipal_pnlModalCadastroPonto')
+
+monthDays = len(tdsEntrada1)
+
+print('Dias neste mês: ' + str(monthDays))
+
+for x in range(monthDays):
+    data = tdsEntrada1[x].get_attribute('dp')
+    diaSemana = tdsDiaSemana[x].get_attribute('innerHTML')
+
+    try:
+        notWorkingAt.index(data)
+        print('Not working at: ' + data + ' - ' + diaSemana)
+    except ValueError:
+        if diaSemana != 'sábado' and not workOnSaturday and diaSemana != 'domingo' and not workOnSunday:
+
+            print('Dia: ' + data + ' - ' + diaSemana)
+
+            # Início do Expediente
+            tdsEntrada1[x].click()
+            time.sleep(4)
+            inputSchedule = browser.find_element(By.ID, 'ctl00_cphPrincipal_txtHorario')
+            inputSchedule.clear()
+            inputSchedule.send_keys('0745')
+
+            btnSalvarHorario = browser.find_element(By.ID, 'ctl00_cphPrincipal_btnCadastroPonto')            
+            btnSalvarHorario.click()
+            print('Waiting Request to Save')
+            time.sleep(5)
+
+            # Início do Almoço
+            tdsSaida1[x].click()
+            time.sleep(4)
+            inputSchedule = browser.find_element(By.ID, 'ctl00_cphPrincipal_txtHorario')
+            inputSchedule.clear()
+            inputSchedule.send_keys('1230')
+
+            btnSalvarHorario = browser.find_element(By.ID, 'ctl00_cphPrincipal_btnCadastroPonto')            
+            btnSalvarHorario.click()
+            print('Waiting Request to Save')
+            time.sleep(5)
+
+            # Fim do Almoço
+            tdsEntrada2[x].click()
+            time.sleep(4)
+            inputSchedule = browser.find_element(By.ID, 'ctl00_cphPrincipal_txtHorario')
+            inputSchedule.clear()
+            inputSchedule.send_keys('1330')
+
+            btnSalvarHorario = browser.find_element(By.ID, 'ctl00_cphPrincipal_btnCadastroPonto')            
+            btnSalvarHorario.click()
+            print('Waiting Request to Save')
+            time.sleep(5)
+
+            # Fim do Expediente
+            tdsSaida2[x].click()
+            time.sleep(4)
+            inputSchedule = browser.find_element(By.ID, 'ctl00_cphPrincipal_txtHorario')
+            inputSchedule.clear()
+            inputSchedule.send_keys('1645')
+
+            btnSalvarHorario = browser.find_element(By.ID, 'ctl00_cphPrincipal_btnCadastroPonto')            
+            btnSalvarHorario.click()
+            print('Waiting Request to Save')
+            time.sleep(5)
